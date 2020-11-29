f0d2155d3a47e463565a88d8c384898a053e00c1
Ferdinand Hanif <ferdiihfz@gmail.com>
2020-11-29 14:17:14 +0700

    feat/sirekrutmenH9 project spring starter

---
+++ SIRekrutmenH9/src/main/java/apap/tk/SIRekrutmenH9/SiRekrutmenH9Application.java
@@ -0,0 +1,13 @@
+package apap.tk.SIRekrutmenH9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiRekrutmenH9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiRekrutmenH9Application.class, args);
+	}
+
+}
---
+++ SIRekrutmenH9/src/test/java/apap/tk/SIRekrutmenH9/SiRekrutmenH9ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tk.SIRekrutmenH9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiRekrutmenH9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
