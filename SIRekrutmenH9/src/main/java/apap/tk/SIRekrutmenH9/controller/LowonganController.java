14d87cfda354af0da27dd2be6e0fd2d60c88209d
SulthanZahran <60386842+SulthanZahran@users.noreply.github.com>
2020-12-03 05:34:45 +0700

    Lto (#8)
    * tambah folder
    
    * feat: LowonganModel
    
    * feat: JenisLowonganModel
    
    * feat: LowonganService
    
    * feat: daftarLowongan
    
    * fix: LowonganModel
    
    * feat: daftarLowongan
    
    * fix: daftarLowongan html
    
    * fix daftarLowongan

--- SIRekrutmenH9/src/main/java/apap/tk/SIRekrutmenH9/controller/LowonganController.java
+++ SIRekrutmenH9/src/main/java/apap/tk/SIRekrutmenH9/controller/LowonganController.java
@@ -32,6 +32,7 @@ public class LowonganController {
     public String daftarLowongan(Model model){
         List<LowonganModel> listLowongan = lowonganService.getLowonganList();
         UserModel userLogin = userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
         Long roleUser = userLogin.getRole().getId();
         String uuidStaffRekrutmen = "";
 
@@ -42,6 +43,7 @@ public class LowonganController {
         model.addAttribute("listLowongan", listLowongan);
         model.addAttribute("roleUser", roleUser);
         model.addAttribute("uuidStaffRekrutmen", uuidStaffRekrutmen);
+
 
         return "daftar-lowongan";
     }
