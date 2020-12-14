849ac8fd7fdab62ce67ab63ab46c9f8d0e30148d
ruka <rokamudrik@gmail.com>
2020-12-14 16:13:51 +0700

    feat: added listLamaran to Pelamar

--- SIRekrutmenH9/src/main/java/apap/tk/SIRekrutmenH9/model/PelamarModel.java
+++ SIRekrutmenH9/src/main/java/apap/tk/SIRekrutmenH9/model/PelamarModel.java
@@ -2,7 +2,9 @@ package apap.tk.SIRekrutmenH9.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -58,6 +61,9 @@ public class PelamarModel implements Serializable {
     @Size(max = 100)
     @Column(name = "alamat")
     String alamat;
+
+    @OneToMany(mappedBy = "pelamarModel", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<LamaranModel> listLamaran;
 
     public UserModel getUuidUser() {
         return this.uuidUser;
