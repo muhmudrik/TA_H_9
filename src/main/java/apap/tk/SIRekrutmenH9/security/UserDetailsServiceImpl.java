f245677e597ab9704d8ab95db5bbdda0bbe6ec52
ruka <rokamudrik@gmail.com>
2020-12-30 18:13:22 +0700

    added: transactional to userDetail

--- src/main/java/apap/tk/SIRekrutmenH9/security/UserDetailsServiceImpl.java
+++ src/main/java/apap/tk/SIRekrutmenH9/security/UserDetailsServiceImpl.java
@@ -12,11 +12,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserDb userDb;
