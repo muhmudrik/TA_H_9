fb6f15e7736f2e4048fa6e4830bd96d3c6c27a51
Muhammad Mudrik <44206861+muhmudrik@users.noreply.github.com>
2020-12-30 23:02:45 +0700

    fix: modif kutipnya

--- src/main/java/apap/tk/SIRekrutmenH9/repository/LamaranDb.java
+++ src/main/java/apap/tk/SIRekrutmenH9/repository/LamaranDb.java
@@ -12,9 +12,9 @@ import apap.tk.SIRekrutmenH9.model.LamaranModel;
 @Repository
 public interface LamaranDb extends JpaRepository<LamaranModel, Long>{
     
-    @Query(value = "SELECT * FROM 'lamaran' WHERE 'id_lowongan' = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM lamaran WHERE id_lowongan = ':id'", nativeQuery = true)
     List<LamaranModel> findByLowonganModel_id_lowongan(@Param("id") Long id_lowongan);
 
-    @Query(value = "SELECT COUNT(*) FROM 'lamaran' WHERE 'id_lowongan' = :lowong AND 'status' = 2", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM lamaran WHERE id_lowongan = ':lowong' AND status = 2", nativeQuery = true)
     Integer findLamaranDiterimaLowongan(@Param("lowong") Long id_lowongan);
 }
